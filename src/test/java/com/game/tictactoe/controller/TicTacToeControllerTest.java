f2733d0c8aea4cb65f82d13d19aafeab010c8a1c
SABBELOO <sarrenabbeloos@gmail.com>
2020-04-06 10:02:07 +0200

    Added test class with http status and view name test methods

---
+++ src/test/java/com/game/tictactoe/controller/TicTacToeControllerTest.java
@@ -0,0 +1,48 @@
+package com.game.tictactoe.controller;
+
+import com.game.tictactoe.util.TicTacToeConstants;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+
+/**
+ * Class for testing all the controller methods
+ *
+ * @Author 2020-DEV-064
+ */
+class TicTacToeControllerTest {
+
+    MockMvc mockMvc;
+    TicTacToeController ticTacToeController;
+
+
+    @BeforeEach
+    void setUp() {
+        this.ticTacToeController = new TicTacToeController();
+
+        mockMvc = MockMvcBuilders.standaloneSetup(ticTacToeController).build();
+    }
+
+    /**
+     * Test http status return equals 200
+     */
+    @Test
+    void testPrintGameLayoutHttp() throws Exception {
+        mockMvc.perform(get(TicTacToeConstants.URI_START_TEMPLATE)).andExpect(status().isOk());
+    }
+
+    /**
+     * Test if view name equals 'tictactoe/index'
+     */
+    @Test
+    void testPrintGameLayoutViewName() throws Exception {
+        mockMvc.perform(get(TicTacToeConstants.URI_START_TEMPLATE))
+                .andExpect(view().name(TicTacToeConstants.EXPECTED_VIEW_NAME));
+    }
+}
