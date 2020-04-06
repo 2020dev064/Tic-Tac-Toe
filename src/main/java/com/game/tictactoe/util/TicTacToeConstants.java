ba654ea68d8419d6b2622ceca1e34834d8915e5c
SABBELOO <sarrenabbeloos@gmail.com>
2020-04-06 10:09:14 +0200

    Added start play field constant

--- src/main/java/com/game/tictactoe/util/TicTacToeConstants.java
+++ src/main/java/com/game/tictactoe/util/TicTacToeConstants.java
@@ -1,8 +1,18 @@
 package com.game.tictactoe.util;
 
+/**
+ * All the constant variables will be stored in this class
+ *
+ * @Author 2020-DEV-064
+ */
 public class TicTacToeConstants {
 
     public static final String URI_START_TEMPLATE = "/start";
 
     public static final String EXPECTED_VIEW_NAME = "tictactoe";
+
+    public static final char[][] START_GAME_PLAY_FIELD = {{' ', '1', '2', '3'},
+            {'1', '-', '-', '-'},
+            {'2', '-', '-', '-'},
+            {'3', '-', '-', '-'}};
 }
