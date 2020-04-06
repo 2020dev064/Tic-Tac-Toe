00edf4a74004b84397ff6c6a9f24361ea24c7203
SABBELOO <sarrenabbeloos@gmail.com>
2020-04-06 12:57:28 +0200

    Added play field initialisation and @Getter annotation

--- src/main/java/com/game/tictactoe/model/GameStatus.java
+++ src/main/java/com/game/tictactoe/model/GameStatus.java
@@ -1,16 +1,18 @@
 package com.game.tictactoe.model;
 
+import com.game.tictactoe.util.TicTacToeConstants;
+import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
 
 /**
- *
  * This class stores the variables needed in the controller
  *
  * @Author 2020-DEV-064
  */
 @Setter
+@Getter
 @Component
 public class GameStatus {
-    private char[][] playField;
+    private char[][] playField = TicTacToeConstants.START_GAME_PLAY_FIELD;;
 }
