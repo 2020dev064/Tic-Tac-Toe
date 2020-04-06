45064fee4ace2f6d00908978db0efbe27aa43a3e
SABBELOO <sarrenabbeloos@gmail.com>
2020-04-06 11:02:26 +0200

    Created NumberNotInRange exception class

---
+++ src/main/java/com/game/tictactoe/exceptions/NumberNotInRangeException.java
@@ -0,0 +1,13 @@
+package com.game.tictactoe.exceptions;
+
+/**
+ * @Author 2020-DEV-064
+ */
+public class NumberNotInRangeException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public NumberNotInRangeException(String message) {
+        super(message);
+    }
+}
