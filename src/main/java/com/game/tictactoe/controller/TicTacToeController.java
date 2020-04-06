0a175ef0b4a7b75bc9eadda9cef363d4183a225e
SABBELOO <sarrenabbeloos@gmail.com>
2020-04-06 10:11:25 +0200

    Updated printGameLayout method to print the starting play field

--- src/main/java/com/game/tictactoe/controller/TicTacToeController.java
+++ src/main/java/com/game/tictactoe/controller/TicTacToeController.java
@@ -1,13 +1,25 @@
 package com.game.tictactoe.controller;
 
+import com.game.tictactoe.util.TicTacToeConstants;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * The class will be controlling the data between the service and the view
+ *
+ * @Author 2020-DEV-064
+ */
 @Controller
 public class TicTacToeController {
 
+    /**
+     * This method will print the game layout of the start page
+     * The key value playfield and TicTacToe constant is used in the index.html by thymeleaf
+     */
     @RequestMapping(value = "/start")
-    public String printGameLayout(){
+    public String printGameLayout(Model model){
+        model.addAttribute("playField", TicTacToeConstants.START_GAME_PLAY_FIELD);
         return "tictactoe";
     }
 }
