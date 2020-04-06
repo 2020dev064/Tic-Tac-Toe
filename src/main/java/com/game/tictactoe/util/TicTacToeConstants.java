c4df9bee1529d8a7856d767e932e208654261877
SABBELOO <sarrenabbeloos@gmail.com>
2020-04-06 10:26:25 +0200

    Added player 'X' and 'O' constants

--- src/main/java/com/game/tictactoe/util/TicTacToeConstants.java
+++ src/main/java/com/game/tictactoe/util/TicTacToeConstants.java
@@ -11,6 +11,10 @@ public class TicTacToeConstants {
 
     public static final String EXPECTED_VIEW_NAME = "tictactoe";
 
+    public static final char PLAYER_ONE_X = 'X';
+
+    public static final char PLAYER_TWO_O = 'O';
+
     public static final char[][] START_GAME_PLAY_FIELD = {{' ', '1', '2', '3'},
             {'1', '-', '-', '-'},
             {'2', '-', '-', '-'},
