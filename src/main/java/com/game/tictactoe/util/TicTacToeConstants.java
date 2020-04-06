b822e0a859c90d81796bb23bc04fb0b9d71e274c
SABBELOO <sarrenabbeloos@gmail.com>
2020-04-06 09:57:38 +0200

    Added constant file with URI and view name constants

---
+++ src/main/java/com/game/tictactoe/util/TicTacToeConstants.java
@@ -0,0 +1,8 @@
+package com.game.tictactoe.util;
+
+public class TicTacToeConstants {
+
+    public static final String URI_START_TEMPLATE = "/start";
+
+    public static final String EXPECTED_VIEW_NAME = "tictactoe";
+}
